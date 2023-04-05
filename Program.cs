d98f39bd0b432420fb3009d417df850a4aaca7c8
alexander22042009 <101943388+alexander22042009@users.noreply.github.com>
2023-04-05 13:11:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                if (i % 2 == 0) evenSum += element;
+                else oddSum += element;
+            }
+ }
+    }
+}
